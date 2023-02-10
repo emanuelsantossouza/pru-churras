5278910f2a2ec9f1dae5bbe401751a2dfbae3ffc
Emanuel Santos Souza de Jesus <emanuel.jesus3@etec.sp.gov.br>
2023-02-10 20:37:29 -0300

    cara ou coroa

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -6,7 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+  moeda = 'assets/logo.png';
+  cara = 'assets/cara.png';
+  coroa = 'assets/coroa.png';
+  info = 'Clique em jogar a moeda!!!';
 
   constructor() {}
+
+  jogarMoeda(){
+    if (Math.random() < 0.5) {
+      this.moeda = this.cara;
+      this.info = 'Cara';
+    } else {
+      this.moeda = this.coroa;
+      this.info = 'Coroa!'
+    }
+  }
 
 }
