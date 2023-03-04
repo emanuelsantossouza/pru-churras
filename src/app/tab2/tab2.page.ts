2f1e4872238e92e8d3ac407c7e2c0cd931b0871f
Emanuel Santos Souza de Jesus <emanuelsantoss42@gmail.com>
2023-03-03 21:12:38 -0300

    Atualização

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+import { async } from 'rxjs';
 
 @Component({
   selector: 'app-tab2',
@@ -7,6 +9,92 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
-  constructor() {}
+  constructor(private alertController: AlertController) {}
 
-}
+  valendo = 1;
+  pontosTime1 = 0;
+  pontosTime2 = 0;
+  time1 = 1;
+  time2 = 1;
+  conta = 0;
+  mais1 = 1;
+  mais3 = 3;
+  mais6 = 6;
+  mais12 = 12; 
+
+
+  handlerMessage = '';
+  roleMessage = '';
+
+
+
+  async Mais(){
+    if(this.valendo <= 11){
+      this.valendo = +1
+    }else {
+        const alert = await this.alertController.create({
+           header: 'Alert!',
+           buttons: [
+            {
+              text: 'Cancel',
+              role: 'cancel',
+              handler: () => {
+                this.handlerMessage = 'Alert canceled';
+              },
+            },
+            {
+              text: 'OK',
+              role: 'confirm',
+              handler: () => {
+                this.handlerMessage = 'Alert confirmed';
+              },
+            },
+          ],
+        });
+      
+        await alert.present();
+      
+        const { role } = await alert.onDidDismiss();
+        this.roleMessage = `Dismissed with role: ${role}`;
+      }
+    }
+
+    Menos(){
+      this.valendo -1 
+    }
+
+    AdicionarMaisUm(){
+      this.valendo = this.mais1
+    }
+
+    AdicionarMaisTres(){
+      this.valendo = this.mais3
+    }
+
+    AdicionarMaisSeis(){
+      if(this.valendo == 11){
+        this.valendo = +1
+        alert("Acabou!!!")
+      }else{
+        this.valendo = +6
+      }
+    }
+
+    AdicionarMaisNove(){
+      if(this.valendo == 11){
+        this.valendo = +1
+        alert("Acabou!!!")
+      }else{
+        this.valendo = +9
+      }
+    }
+
+    AdicionarMaisDoze(){
+      if(this.valendo <= 11){
+        this.valendo = +1
+        alert("Acabou!!!")
+      }else{
+        this.valendo = +12
+      }
+    }
+  }
